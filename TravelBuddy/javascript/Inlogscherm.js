8a6065a623132e494eadcd741e6a357a5f7f7201
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-08 20:25:29 +0200

    Travelbuddy geupload

---
+++ TravelBuddy/javascript/Inlogscherm.js
@@ -0,0 +1,6 @@
+console.log('bakkerij.html');
+
+/* <!-- https://www.codegrepper.com/code-examples/html/how+to+link+button+to+another+page+in+javascript --> */
+function btnClicked(){
+    document.location.href="../Filterscherm1.html";
+}
