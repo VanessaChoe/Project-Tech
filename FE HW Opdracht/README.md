fbabcbd415a8b00aed4890a6c26b6d05876c4ceb
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-10 22:32:48 +0200

    deleted files, localhost, Travelbuddies

--- FE HW Opdracht/README.md
+++
@@ -1,4 +0,0 @@
-# FED2 21/22 - Datatypes - exercises
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/vanessachoe/pen/MWQgaae](https://codepen.io/vanessachoe/pen/MWQgaae).
-
--- TravelBuddy/static/Inlogscherm.js
+++ TravelBuddy/static/Inlogscherm.js
@@ -2,5 +2,5 @@ console.log('bakkerij.html');
 
 /* <!-- https://www.codegrepper.com/code-examples/html/how+to+link+button+to+another+page+in+javascript --> */
 function btnClicked(){
-    document.location.href="../Filterscherm1.html";
+    document.location.href="Filterscherm1.html";
 }
