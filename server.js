81050de1e23afa8ad92e93833b00dcd35a50cea7
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-11 08:29:05 +0200

    Hello Worldgit help

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Webserver running on port localhost:${port}`)
 })
+
+// app.use('/static', express.static('public'));
+// http://localhost:3000/static/index.html
+
