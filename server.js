7f9b153f5e797c24b4c7b81ab0bc713beb46260b
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-11 18:48:41 +0200

    server image

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
+// const express = require('express');
+
+// ////////////////////////////////////////////////
+// const app = express()
+// const port = 3000
+// const path = require("path");
+
+// const publicpath = path.resolve(__dirname,'public')
+// ///////////////////////////////////////////////
+
+// app.use('publicpath',express.static('Images'));
+
+// {/* <img src="public/America.jpg"></img> */}
+
+// app.get('/',function(req, res){
+//   res.send('Static Files')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Webserver running on port localhost:${port}`)
+// })
+
+
+// http://localhost:3000/Inlogscherm.html
+
 const express = require('express');
 
 // ////////////////////////////////////////////////
 const app = express()
 const port = 3000
-const path = require("path");
 
-const publicpath = path.resolve(__dirname,'public')
-// ///////////////////////////////////////////////
+app.use(express.static('public'));
 
-app.use('publicpath',express.static('Images'));
-
-<img src="public/America.jpg"></img>
-/////////////////////////////////////////////////
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 app.get('/',function(req, res){
-  res.send('Static Files')
+   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Webserver running on port localhost:${port}`)
-})
-
-
-// http://localhost:3000/Inlogscherm.html
-
+   console.log(`Webserver running on port localhost:${port}`)
