93bbaac3d6bd90e4a520458d3d233ec08905f1e9
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-11 19:07:33 +0200

    ghg

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.use(express.static('Project-Tech'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
    res.send('Hello World!')
