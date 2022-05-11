6981cf35026d138fb76e64f7da2de0d4cca2c7f1
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-11 18:21:23 +0200

    Added image

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require('express')
 // ////////////////////////////////////////////////
 const app = express()
 const port = 3000
-const path = require('favorieten')
-
 // ///////////////////////////////////////////////
 
-app.use(express.static('public'));
+app.use('/public',express.static('Images'));
 
+<img src= "/Images/America.jpg"></img>
 /////////////////////////////////////////////////
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.get('/',function(req, res){
+  res.send('Static Files')
 })
 
 app.listen(port, () => {
