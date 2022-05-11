8d3bc22a6a024ae6801acc256789736fc38ab8a1
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-11 18:54:31 +0200

    changed path

--- server.js
+++ server.js
@@ -29,11 +29,12 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+app.use(express.static('Project-Tech'));
 
-app.get('/',function(req, res){
+app.get('/', (req, res) => {
    res.send('Hello World!')
 })
 
 app.listen(port, () => {
    console.log(`Webserver running on port localhost:${port}`)
+})
