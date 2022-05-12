aba6b308d03096ea26f1c376510d8fc94dee1964
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-12 08:34:01 +0200

    downloaded squirrelly

--- server.js
+++ server.js
@@ -20,11 +20,22 @@
 //   console.log(`Webserver running on port localhost:${port}`)
 // })
 
+
+
+
+
+
+
+
+
+
+import * as Sqrl from 'squirrelly'
+
 const myTemplate = 'My favorite template engine is {{it.favorite}}.'
 
-// const result = Sqrl.render(myTemplate, {
-//   favorite: 'Squirrelly, definitely'
-// })
+const result = Sqrl.render(myTemplate, {
+  favorite: 'Squirrelly, definitely'
+})
 
 const express = require('express');
 
@@ -49,11 +60,6 @@ app.get('/login', (req, res) => {
 app.listen(port, () => {
    console.log(`Webserver running on port localhost:${port}`)
 })
-
-/* <!-- https://www.codegrepper.com/code-examples/html/how+to+link+button+to+another+page+in+javascript --> */
-// function btnClicked(){
-//   document.location.href="filterscherm1.html";
-// }
 
 
 
