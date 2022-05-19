09f28a0f22909d0ced08760e1baf91ce732168ba
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-19 22:36:44 +0200

    connected to MongoDB?

--- server.js
+++ server.js
@@ -41,10 +41,11 @@
 // 	console.log(`Webserver running on port localhost:${port}`)
 // })
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const dotenv = require('dotenv');
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const port = 3000;
+// const dotenv = require('dotenv');
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = "mongodb+srv://VanessaChoe:<Jovugi81!>@cluster0.quc5i.mongodb.net/?retryWrites=true&w=majority";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -53,6 +54,23 @@ const dotenv = require('dotenv');
   // perform actions on the collection object
 //   client.close();
 // });
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express';
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://VanessaChoe:ster12345@cluster0.quc5i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+const path = require('path');
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -66,4 +84,8 @@ app.get('/mijnTravelBuddiesMatches', (req, res) => {
 
 app.post ("/geslacht", (req, res) => {
   res.send("geslacht")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
