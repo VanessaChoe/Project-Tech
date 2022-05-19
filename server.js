f1eaefb5c7e1dc333bce57e29863a285c5a14ef5
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-19 09:29:24 +0200

    new push heroku

--- server.js
+++ server.js
@@ -44,14 +44,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://VanessaChoe:<Jovugi81!>@cluster0.quc5i.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://VanessaChoe:<Jovugi81!>@cluster0.quc5i.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
-});
+//   client.close();
+// });
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -59,9 +60,13 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
   res.render('homepage')
 })
-// app.get('/mijnTravelBuddiesMatches', (req, res) => {
-// 	res.render('mijnTravelBuddiesMatches')
-//   })
+app.get('/mijnTravelBuddiesMatches', (req, res) => {
+  res.render('mijnTravelBuddiesMatches')
+})
+
+app.post ("/geslacht", (req, res) => {
+  res.send("geslacht")
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
