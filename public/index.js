c54e4e77898ae29fa0d86c59f9add02765219f4f
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-24 22:28:51 +0200

    changed active state

--- public/index.js
+++ public/index.js
@@ -1,36 +1,12 @@
 console.log("index.js");
 
-// var NieuweTravelBuddyButton = document.querySelector('#NieuweTravelBuddyButton');
+var label = document.getElementById('Vrouw');
 
-// var keuzeSection = document.querySelector('#keuzeSection');
-// var geslachtSection = document.querySelector('#geslachtSection');
+label.addEventListener('click', scrollToSection);
 
-// function verderNaarGeslacht() {
-//     console.log("verder naar filteren op geslacht");
-//     keuzeSection.src = geslachtSection.src;
-// }
-
-// NieuweTravelBuddyButton.addEventListener('click' , verderNaarGeslacht) ;
-
-// document.getElementById("NieuweTravelBuddyButton").onclick = function () {
-//     location.href = "#geslachtSection";
-// };
-
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function scrollToSection() {
+	console.log(window.location.href)
+	
+	window.location.href = 
+	`${window.location.href}#continentenSection`
 }
--- server.js
+++ server.js
@@ -1,83 +1,16 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-// const path = require('path');
-
-// app.use(express.static("public"));
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-// 	res.render("Travelbuddy");
-// });
-// app.get("/matchTravelBuddy", (req, res) => {
-//  	res.render("matchTravelBuddy", {
-// 	});
-// app.get("/geslacht", (req, res) => {
-
-// 	const match = {
-// 		image: "/images/eline.jpg",
-// 		data: "Eline - Afrika, Marokko",
-
-// 	}
-
-// 	res.render("geslacht",{matchEline:match});
-// });
-// app.get("/filterschermGeslacht", (req, res) => {
-// 	res.render("filterschermGeslacht");
-// });
-// app.get("/filterschermContinenten", (req, res) => {
-// 	res.render("filterschermContinenten");
-// });
-// app.get("/matchTravelBuddy", (req, res) => {
-// 	res.render("matchTravelBuddy", {
-// 		data: "iets",
-// 	});
-// });
-
-// app.listen(port, () => {
-// 	console.log(`Webserver running on port localhost:${port}`)
-// })
-
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3000;
-// const port = 3000;
-// const dotenv = require('dotenv');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://VanessaChoe:<Jovugi81!>@cluster0.quc5i.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-//   client.close();
-// });
-
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-// import { Client } from 'best-error-reporting-service'
-
-// export default new Client(process.env.API_KEY)
-
-// import 'dotenv/config'
-
-// import errorReporter from './errorReporter.mjs'
-// errorReporter.report(new Error('documented example'))
-
 // mongodb connectie///////////////////////////////////////////////////////////////////////
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 const path = require('path');
 
@@ -96,6 +29,10 @@ app.get('/mijnTravelBuddiesMatches', (req, res) => {
   // });
 });
 
+// app.get("/geslacht", async (req, res) => {
+//   res.render("")
+// }
+// )
 app.post ("/geslacht", (req, res) => {
   res.send("geslacht")
 })
