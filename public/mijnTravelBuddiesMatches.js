27cb46bfa8296887f6630c558b07f3cf3ab5fedd
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-25 21:00:11 +0200

    added a public api

--- public/mijnTravelBuddiesMatches.js
+++ public/mijnTravelBuddiesMatches.js
@@ -4,3 +4,18 @@ document.getElementById("ButtonTerug").onclick = function () {
 document.getElementById("ButtonNavUitloggen").onclick = function () {
 	location.href = "/";
 };
+
+function initMap(){
+	var options = {
+		zoom:8,
+		center: {lat:52.361966,lng:4.907146}
+	}
+
+	var map = new google.maps.Map(document.getElementById('map'), options);
+
+	var marker = new google.maps.Marker({
+		position: {lat:52.361966,lng:4.907146},
+		map: map,
+		icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+	});
+}
