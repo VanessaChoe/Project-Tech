f4d9687dcb16aa6da4fdb1bf5d2a5de46626cdc1
VanessaChoe <vanessa.choe.12@gmail.com>
2022-05-31 22:11:08 +0200

    onnodige code weggehaald

--- public/index.js
+++ public/index.js
@@ -21,30 +21,4 @@ function scrollToFirstSection() {
 	window.location.hash     = 'continenten';
 }
 
-// const continentenButtons = document.getElementById('continenten');
-// continentenButtons.forEach(naarMatchButton);
-
-// function naarMatchButton() {
-
-// 	window.location.hash     = 'trigger';
-// }
-
-/* //Popup https://sabe.io/tutorials/how-to-create-modal-popup-box/////////////////////////////////////////// */
-
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
