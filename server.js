44c2ab704d4f90aeb60f5555eb2115e36d14f9f8
Vanessa Choe <vanessa.choe.12@gmail.com>
2022-06-30 06:01:21 +0200

    added path in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const path = require('path')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
